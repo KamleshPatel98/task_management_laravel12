470f102bf0bc23ed5d669a97c977ddd34801136b
KamleshPatel98 <kamleshp52170@gmail.com>
2025-07-04 18:43:01 +0530

    composer require laravel/slack-notification-channel

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
+use Illuminate\support\Facades\Notification;
+use App\Notifications\TestNotification;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    Notification::route('slack', env('SLACK_URL'))->notify(new TestNotification);
+    return view('welcome');
+});
 
 Route::resource('users',UserController::class);
 Route::resource('projects',ProjectController::class);
